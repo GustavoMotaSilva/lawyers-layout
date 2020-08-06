fbde153f0b0b9bfc570a44867bde2e049502a7ad
Lucas Akira Ayabe <lucas.ayabe@gmail.com>
2020-08-05 22:01:02 -0300

    added smooth scroll

--- script.js
+++ script.js
@@ -7,7 +7,14 @@ const fromEvent = (event) => (element) => (callback) => {
     return element.addEventListener(event, callback);
 };
 
+const fromEventArray = (event) => (elements) => (callback) => {
+    return elements.forEach((element) => {
+        element.addEventListener(event, callback);
+    });
+};
+
 const fromClick = fromEvent('click');
+const fromClickArray = fromEventArray('click');
 
 const fromScroll = fromEvent('scroll');
 
@@ -18,12 +25,21 @@ const fromScroll = fromEvent('scroll');
  */
 const select = (selector) => document.querySelector(selector);
 
+/**
+ *
+ * @param {string} selector
+ * @returns {NodeListOf<HTMLElement>}
+ */
+const selectAll = (selector) => document.querySelectorAll(selector);
+
 const navbar = select('.navbar');
 const menu = select('.menu[aria-expanded]');
 const hamburger = select('.hamburger');
+const internalLinks = selectAll('.menu .link[href^="#"][data-internal]');
 
 const fromScrollInWindow = fromScroll(window);
 const fromClickInHamburger = fromClick(hamburger);
+const fromClickInInternalLinks = fromClickArray(internalLinks);
 
 const toggleNavbar = () => {
     if (window.scrollY > 100) {
@@ -33,11 +49,23 @@ const toggleNavbar = () => {
     }
 };
 
-// toggleNavbar();
-fromScrollInWindow(toggleNavbar);
-
-fromClickInHamburger(() => {
+const toggleHamburger = () => {
     const isMenuActive = menu.getAttribute('aria-expanded') === 'true';
     hamburger.classList.toggle('is-active');
     menu.setAttribute('aria-expanded', isMenuActive ? 'false' : 'true');
-});
+};
+
+const smoothScroll = (event) => {
+    event.preventDefault();
+    toggleHamburger();
+
+    const linkTarget = select(event.target.hash);
+    linkTarget.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+    });
+};
+
+fromScrollInWindow(toggleNavbar);
+fromClickInHamburger(toggleHamburger);
+fromClickInInternalLinks(smoothScroll);
