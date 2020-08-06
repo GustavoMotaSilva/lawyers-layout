1c559dffadeafc044afaa98b67dcedf02b3c0d4e
Lucas Akira Ayabe <lucas.ayabe@gmail.com>
2020-08-05 21:28:10 -0300

    final template

--- script.js
+++ script.js
@@ -25,13 +25,16 @@ const hamburger = select('.hamburger');
 const fromScrollInWindow = fromScroll(window);
 const fromClickInHamburger = fromClick(hamburger);
 
-fromScrollInWindow(() => {
-    if (window.scrollY > 10) {
+const toggleNavbar = () => {
+    if (window.scrollY > 100) {
         navbar.classList.add('is-active');
     } else {
         navbar.classList.remove('is-active');
     }
-});
+};
+
+// toggleNavbar();
+fromScrollInWindow(toggleNavbar);
 
 fromClickInHamburger(() => {
     const isMenuActive = menu.getAttribute('aria-expanded') === 'true';
